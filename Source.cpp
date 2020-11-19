d22582a5f664104c9dd5dba413841d12a2179215
Nicole M <35404943+nicomcd@users.noreply.github.com>
2020-11-18 21:25:50 -0500

    The one that started it all
    For the sake of history

--- Source.cpp
+++
@@ -1,8 +0,0 @@
-//    I pledge my honor that I have abided by the Stevens Honor System
-#include<iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
